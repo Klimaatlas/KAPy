1a82057ad9da2fc20844aa86522efd32c3dccf30
Mark R. Payne <mapa@dmi.dk>
2024-03-07 10:38:08 +0100

    Move to the snakemake configuration where possible. Remove use of defaults file

--- workflow/KAPy/helpers.py
+++ workflow/KAPy/helpers.py
@@ -28,33 +28,16 @@ def buildPath(cfg,thisDir, files=[]):
     return(rtn)
         
 
-def loadConfig(configfile='config.yaml',
-               useDefaults=True,
-               defaultFile='config/defaults.yaml'):
+def loadConfig(configfile='config.yaml'):
     '''
     Load config file
     
-    Loads the KAPy config file specified in the yaml format. The use of system-wide defaults can
-    be disabled with the 'useDefualts' switch - the path for the defaults is specified in 
-    'defaultFile'
+    Loads the KAPy config file specified in the yaml format.
     '''
+    
     #Load config file
     with open(configfile, 'r') as f:
     	cfg=yaml.safe_load(f)
     
-    #If we are using defaults, load them as well and then merge the two dicts
-    if useDefaults:
-        #Load defaults
-        with open(defaultFile, 'r') as f:
-            dft=yaml.safe_load(f)
-        #Previously we have done a partial update of the defaults, using a 
-        #recursive function that digs down below the top level. This doesn't
-        #work very well unfortunately, particularly when you want to exclude 
-        #elements. Here we use a simple top-level replacement instead
-        rtn=dft
-        for key,values in cfg.items():
-            rtn[key]=values
-        return(rtn)
-    else:
-        return(cfg)
+    return(cfg)
 
