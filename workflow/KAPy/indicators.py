220cc7a90e0519c9849101af693bf12fad51f123
Mark R. Payne <mapa@dmi.dk>
2024-03-13 08:20:03 +0100

    Handle case where periods don't overlap with dataset

--- workflow/KAPy/indicators.py
+++ workflow/KAPy/indicators.py
@@ -1,12 +1,19 @@
+"""
+#Setup for debugging with a Jupyterlab console
+import os
+os.chdir("..")
+import KAPy
+os.chdir("..")
+config=KAPy.loadConfig()  
+inFile=['results/1.primVars/tas_CORDEX_rcp85_AFR-22_MPI-M-MPI-ESM-LRr1i1p1_GERICS-REMO2015_v1_mon.nc']
+indID=101
+"""
+
 import pickle
 import xarray as xr
 import os
 import numpy as np
 import sys
-
-#config=KAPy.loadConfig()
-#inFile=['./workDir/2.primVars/tas_CORDEX_rcp45_AFR-22_MPI-M-MPI-ESM-LR_r1i1p1_GERICS-REMO2015_v1_mon.nc']
-#thisInd=config['indicators'][101]
 
 def calculateIndicators(config,inFile,outFile,thisInd):
     
@@ -18,15 +25,14 @@ def calculateIndicators(config,inFile,outFile,thisInd):
         with open(inFile[0],'rb') as f:
             thisDat=pickle.load(f)
     else:
-        sys.exit('Unknown file format, "' + inExt +'" in ' + inFile[0])
-        
+        sys.exit(f'Unknown file format, {inExt} in {inFile[0]}')
 
     #Filter by season first (should always work)
     theseMonths=config['seasons'][thisInd['season']]['months']
     datSeason=thisDat.sel(time=np.isin(thisDat.time.dt.month,theseMonths))
         
     #Time binning over periods
-    if thisInd['time_binning'].lower()=='periods':
+    if thisInd['time_binning']=='periods':
         slices=[]
         periodIds=[]
         for thisPeriod in config['periods'].values():
@@ -34,8 +40,12 @@ def calculateIndicators(config,inFile,outFile,thisInd):
                 timemin=datSeason.time.dt.year >=thisPeriod['start']
                 timemax=datSeason.time.dt.year <=thisPeriod['end']
                 datPeriodSeason=datSeason.sel(time=timemin & timemax)
+                #If there is nothing left, we want a result all the same so that we
+                #can put it in the outputs
+                if datPeriodSeason.time.size==0:
+                    res=datPeriodSeason.drop_dims('time')
                 #Apply the operator
-                if thisInd['statistic']=='mean':
+                elif thisInd['statistic']=='mean':
                     res=datPeriodSeason.mean(['time'],keep_attrs=True)
                 else:
                     sys.exit('Unknown indicator statistic, "' + ind['statistic'] +'"')
@@ -47,11 +57,11 @@ def calculateIndicators(config,inFile,outFile,thisInd):
         dout=dout.assign_coords(period=periodIds)
 
     #Time binning by defined units
-    elif thisInd['time_binning'].lower() in ['years','months']:
+    elif thisInd['time_binning'] in ['years','months']:
         #Then group by time
-        if thisInd['time_binning'].lower()=="years":
+        if thisInd['time_binning']=="years":
             datGroupped=datSeason.resample(time="1Y",label="right")
-        elif thisInd['time_binning'].lower()=="months":
+        elif thisInd['time_binning']=="months":
             datGroupped=datSeason.resample(time="1M",label="right")
         else:
             sys.exit("Shouldn't be here")
