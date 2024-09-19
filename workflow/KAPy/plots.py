7f47a20973c43b084e9935351401a17e233f9397
ShingiNangombe <63850110+ShingiNangombe@users.noreply.github.com>
2024-09-19 12:26:30 +0200

    Update plots.py

--- workflow/KAPy/plots.py
+++ workflow/KAPy/plots.py
@@ -174,7 +174,7 @@ def makeLineplot(config, indID, srcFiles, outFile=None):
 
     # Get metafra data from configuration
     scTbl = pd.DataFrame.from_dict(config["scenarios"], orient="index")
-    scColourDict = {x["id"]: x["colour"] for i, x in scTbl.iterrows()}
+    scColourDict = {x["id"]: "#" + x["hexcolour"] for i, x in scTbl.iterrows()}
 
     # Now select data for plotting - we only plot the central value, not the full range
     pltDat = datdf[datdf["percentiles"] == config["ensembles"]["centralPercentile"]]
