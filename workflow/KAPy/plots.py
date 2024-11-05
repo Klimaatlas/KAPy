dc1b54476cdfa4a7d02dead303ccc5c308c3fe4d
Mark R. Payne <mapa@dmi.dk>
2024-11-05 12:26:13 +0100

    Improve labelling of plots

--- workflow/KAPy/plots.py
+++ workflow/KAPy/plots.py
@@ -25,10 +25,8 @@ matplotlib.use('Agg')
 
 # Boxplot---------------------------------------------------------------------------
 """
-indID="101"
-srcFiles=list(wf['plots'].values())[0]
-srcFiles=['results/5.areal_statistics/101_CORDEX_Ghana025_rcp26_ensstats.csv',
-          'results/5.areal_statistics/101_CORDEX_Ghana025_rcp85_ensstats.csv']
+outFile='outputs/7.plots/101_boxplot.png'
+srcFiles=wf['plots'][outFile]
 """
 
 def makeBoxplot(config, indID, srcFiles, outFile=None):
@@ -42,11 +40,15 @@ def makeBoxplot(config, indID, srcFiles, outFile=None):
         datIn = pd.read_csv(f)
         datIn["fname"] = os.path.basename(f)
         datIn["periodID"] = [str(x) for x in datIn["periodID"]]
+        datIn["source"] = datIn["fname"].str.extract("^[^_]+_([^_]+)_[^_]+_[^_]+_.*$")
         datIn["experiment"] = datIn["fname"].str.extract("^[^_]+_[^_]+_[^_]+_([^_]+)_.*$")
         dat += [datIn]
     datdf = pd.concat(dat)
+    datdf['lbl']=[ rw['source'] + "-" + rw['experiment'] if rw['experiment']!='no-expt' else rw['source']
+                  for idx,rw in datdf.iterrows()]
 
-    # Get metafra data from configuration
+
+    # Get metadata from configuration
     ptileTbl = (
         pd.DataFrame.from_dict(
             config["ensembles"], orient="index", columns=["percentiles"]
@@ -63,7 +65,7 @@ def makeBoxplot(config, indID, srcFiles, outFile=None):
     # Now merge into dataframe and pivot for plotting
     pltLong = pd.merge(datdf, ptileTbl, on="percentiles", how="left")
     pltDatWide = pltLong.pivot_table(
-        index=["experiment", "periodID"], columns="ptileLbl", values="mean"
+        index=["lbl", "periodID"], columns="ptileLbl", values="mean"
     ).reset_index()
 
     # Now plot
@@ -72,7 +74,7 @@ def makeBoxplot(config, indID, srcFiles, outFile=None):
         + geom_boxplot(
             mapping=aes(
                 x="periodID",
-                fill="experiment",
+                fill="lbl",
                 middle="centralPercentile",
                 ymin="lowerPercentile",
                 ymax="upperPercentile",
@@ -86,7 +88,7 @@ def makeBoxplot(config, indID, srcFiles, outFile=None):
             x="Period",
             y=f"Value ({thisInd['units']})",
             title=f"{thisInd['name']} ",
-            fill="Experiment",
+            fill="",
         )
         + scale_x_discrete(labels=periodLblDict)
         + theme_bw()
@@ -102,8 +104,8 @@ def makeBoxplot(config, indID, srcFiles, outFile=None):
 
 # Spatialplot -----------------------------------------------------------
 """
-indID='101'
-srcFiles=list(wf['plots'].values())[1]
+outFile='outputs/7.plots/101_spatial.png'
+srcFiles=wf['plots'][outFile]
 """
 def makeSpatialplot(config, indID, srcFiles, outFile=None):
     # Extract indicator info
@@ -118,9 +120,20 @@ def makeSpatialplot(config, indID, srcFiles, outFile=None):
         firstlast = thisdat.isel(periodID=[0,-1])
         firstlastdf = firstlast.indicator_mean.to_dataframe().reset_index()
         firstlastdf["fname"] = os.path.basename(d)
+        firstlastdf["source"] = firstlastdf["fname"].str.extract("^[^_]+_([^_]+)_[^_]+_[^_]+_.*$")
         firstlastdf["experiment"] = firstlastdf["fname"].str.extract("^[^_]+_[^_]+_[^_]+_([^_]+)_.*$")
         datdf += [firstlastdf]
     pltDat = pd.concat(datdf)
+    pltDat['lbl']=[ rw['source'] + "-" + rw['experiment'] if rw['experiment']!='no-expt' else rw['source']
+                  for idx,rw in pltDat.iterrows()]
+
+    #Setup period labelling
+    periodTbl = pd.DataFrame.from_dict(config["periods"], orient="index")
+    periodLblDict = {
+        x["id"]: f"{x['name']}\n({x['start']}-{x['end']})"
+        for i, x in periodTbl.iterrows()
+    }
+    pltDat['periodLbl']= [ periodLblDict[p] for p in pltDat['periodID']]
 
     #Identify spatial coordinates
     spDimX=[d for d in pltDat.columns if d in ['x','longitude','long','lon']]
@@ -132,7 +145,7 @@ def makeSpatialplot(config, indID, srcFiles, outFile=None):
     p = (
         ggplot(pltDat, aes(x="x", y="y", fill="indicator_mean"))
         + geom_raster()
-        + facet_grid("periodID~experiment")
+        + facet_grid("periodLbl~lbl")
         + theme_bw()
         + labs(
             x="",
@@ -155,8 +168,8 @@ def makeSpatialplot(config, indID, srcFiles, outFile=None):
 
 # Lineplot------------------------------------------------------------------
 """
-indID=101
-srcFiles=list(wf['plots'].values())[0]
+outFile='outputs/7.plots/102_lineplot.png'
+srcFiles=wf['plots'][outFile]
 """
 
 
@@ -169,24 +182,28 @@ def makeLineplot(config, indID, srcFiles, outFile=None):
     for f in srcFiles:
         datIn = pd.read_csv(f)
         datIn["fname"] = os.path.basename(f)
+        datIn["source"] = datIn["fname"].str.extract("^[^_]+_([^_]+)_[^_]+_[^_]+_.*$")
         datIn["experiment"] = datIn["fname"].str.extract("^[^_]+_[^_]+_[^_]+_([^_]+)_.*$")
         dat += [datIn]
     datdf = pd.concat(dat)
     #Use datetime library to handle dates longer than 2262 and plotting in plotnine
     datdf["datetime"] = [datetime.strptime(d,"%Y-%m-%d") for d in datdf["time"]]
+    datdf['lbl']=[ rw['source'] + "-" + rw['experiment'] if rw['experiment']!='no-expt' else rw['source']
+                  for idx,rw in datdf.iterrows()]
 
     # Now select data for plotting - we only plot the central value, not the full range
     pltDat = datdf[datdf["percentiles"] == config["ensembles"]["centralPercentile"]]
 
+
     # Now plot
     p = (
-        ggplot(pltDat, aes(x="datetime", y="mean", colour="experiment"))
+        ggplot(pltDat, aes(x="datetime", y="mean", colour="lbl"))
         + geom_point()
         + labs(
             x="",
             y=f"Value ({thisInd['units']})",
             title=f"{thisInd['name']} ",
-            colour="Scenario",
+            colour="",
         )
         + theme_bw()
         + scale_x_datetime(date_labels="%Y")
