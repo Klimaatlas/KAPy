dffe8486f7713521282cc905bcee8b1d261a9657
Mark R. Payne <mapa@dmi.dk>
2026-05-08 16:33:22 +0200

    Switch to a common pipeline-level dask cluster #133

--- workflow/KAPy/dask.py
+++ workflow/KAPy/dask.py
@@ -1,29 +1,40 @@
 from dask.distributed import Client, LocalCluster
+import logging
 import os
 
-def setupDask(nWorkers=None,threadsPerWorker=None,memoryLimit=None):
+def setupDaskCluster(host,
+                     port,
+                     nWorkers,
+                     threadsPerWorker,
+                     memLim):
 
-    # Prevent nested threading leading to oversubscription
-    # ChatGPT is very insistent about this being a good idea.
-    os.environ["OMP_NUM_THREADS"] = "1"
-    os.environ["MKL_NUM_THREADS"] = "1"
-    os.environ["OPENBLAS_NUM_THREADS"] = "1"
+    logging.getLogger("distributed").setLevel(logging.WARNING)
 
-    # Setup cluster
-    # Note the handling of values if not specified is as follows (based on LocalCluster definition:
-    # https://docs.dask.org/en/stable/deploying-python.html#reference)
-    # * n_workers: default is None
-    # * threads_per_worker: default is None
-    # * memory_limit: None 
+    try:
+        #Try to connect to a client, to test if it exists
+        #If successful, don't need to recreate
+        client = Client(f"{host}:{port}", timeout="2s")
 
-    cluster = LocalCluster(
-        n_workers=nWorkers,
-        threads_per_worker=threadsPerWorker,
-        memory_limit=f"{memoryLimit/nWorkers}MB",
-    )
+    except OSError:
+        # Prevent nested threading leading to oversubscription
+        # ChatGPT is very insistent about this being a good idea.
+        os.environ["OMP_NUM_THREADS"] = "1"
+        os.environ["MKL_NUM_THREADS"] = "1"
+        os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
-    client = Client(cluster)
+        #Configure cluster
+        if memLim is None:
+            memoryPerWorker="auto"
+        else:
+            memoryPerWorker=f"{memLim/threadsPerWorker/nWorkers}MB"
+        cluster = LocalCluster(scheduler_port=port,
+                                host=host,
+                            threads_per_worker=threadsPerWorker,
+                            n_workers=nWorkers,
+                            memory_limit=memoryPerWorker)
+        client = Client(cluster)
 
-    print(cluster)
+    print(f"Dask client configuration: {client}")
+    print(f"Dashboard address: {client.dashboard_link}")
 
     return client
