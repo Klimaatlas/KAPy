53f4e9044d4a122c0a25fda8c1ba9f96912dbb66
Mark R. Payne <mapa@dmi.dk>
2026-05-13 18:03:21 +0200

    Move KAPy library back to workflow so that it works in a module #181

--- workflow/KAPy/config.py
+++ workflow/KAPy/config.py
@@ -48,7 +48,7 @@ def validateConfig(config):
     # schemaDir="./workflow/schemas/"
     # schemaDir="./KAPy/workflow/schemas/"
     schemaDir = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), "..","workflow", "schemas"
+        os.path.dirname(os.path.abspath(__file__)), "..", "schemas"
     )
 
     # Do custom validation handling rather the using Snakemake's. The goal
