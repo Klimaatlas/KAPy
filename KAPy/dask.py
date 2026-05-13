bd03f603fd7adf56d35d82f0d04b20e5d3f63579
Mark R. Payne <mapa@dmi.dk>
2026-05-13 17:47:51 +0200

    Implement dask parameters for biasAdjustment script: #133

--- KAPy/dask.py
+++ KAPy/dask.py
@@ -1,47 +1,30 @@
 from dask.distributed import Client, LocalCluster
-from urllib.parse import urlparse
 import logging
 import os
 
-def setupDaskCluster(host,
-                     port,
-                     threads,
+def setupDaskCluster(nWorkers,
                      threadsPerWorker,
                      memoryPerWorker):
 
     logging.getLogger("distributed").setLevel(logging.WARNING)
 
-    try:
-        #Try to connect to a client, to test if it exists
-        #If successful, don't need to recreate
-        client = Client(f"{host}:{port}", timeout="2s")
+    # Prevent nested threading leading to oversubscription
+    # ChatGPT is very insistent about this being a good idea.
+    os.environ["OMP_NUM_THREADS"] = "1"
+    os.environ["MKL_NUM_THREADS"] = "1"
+    os.environ["OPENBLAS_NUM_THREADS"] = "1"
 
-    except OSError:
-        # Prevent nested threading leading to oversubscription
-        # ChatGPT is very insistent about this being a good idea.
-        os.environ["OMP_NUM_THREADS"] = "1"
-        os.environ["MKL_NUM_THREADS"] = "1"
-        os.environ["OPENBLAS_NUM_THREADS"] = "1"
+    #Configure cluster
+    cluster = LocalCluster(threads_per_worker=threadsPerWorker,
+                           n_workers=nWorkers,
+                           memory_limit=memoryPerWorker)
 
-        #Configure cluster
-        cluster = LocalCluster(scheduler_port=port,
-                                host=host,
-                            threads_per_worker=threadsPerWorker,
-                            n_workers=max(1, int(threads/threadsPerWorker)),
-                            memory_limit=memoryPerWorker)
+    #We're good. Make the client
+    client = Client(cluster)
 
-        #Check that resulting cluster is on the correct port
-        actual = urlparse(cluster.scheduler_address)
-        actual_port = actual.port
-        if actual_port != port:
-            raise RuntimeError(f"Cannot assign requested port: {port}")
-
-        #We're good. Make the client
-        client = Client(cluster)
-
-
-    #Print some outputs
-    print(f"Dask client configuration: {client}")
-    print(f"Dashboard address: {client.dashboard_link}")
+    #Save logs
+    logger = logging.getLogger(__name__)
+    logger.info("Dask client configuration: %s", client)
+    logger.info("Dashboard address: %s", client.dashboard_link)
 
     return client
--- workflow/scripts/_03_biasAdjust.py
+++ workflow/scripts/_03_biasAdjust.py
@@ -1,6 +1,37 @@
+# import KAPy
+
+# #Configure dask 
+# config=snakemake.config
+# client=KAPy.setupDaskCluster(nWorkers=config['advanced']['nWorkers'],
+#                      threadsPerWorker=config['advanced']['threadsPerWorker'],
+#                      memoryPerWorker=config['advanced']['memoryPerWorker'])
+
+# #Perform bias adjustment
+# BA=KAPy.biasAdjust(target_file=snakemake.input.target,
+#                     reference_file=snakemake.input.ref,
+#                     **snakemake.params)
+# KAPy.write_variables(BA,dict(snakemake.output))
+
 import KAPy
 
-BA=KAPy.biasAdjust(target_file=snakemake.input.target,
-                    reference_file=snakemake.input.ref,
-                    **snakemake.params)
-KAPy.write_variables(BA,dict(snakemake.output))
+def main():
+    config = snakemake.config
+
+    client = KAPy.setupDaskCluster(
+        nWorkers=config['advanced']['nWorkers'],
+        threadsPerWorker=config['advanced']['threadsPerWorker'],
+        memoryPerWorker=config['advanced']['memoryPerWorker']
+    )
+
+    BA = KAPy.biasAdjust(
+        target_file=snakemake.input.target,
+        reference_file=snakemake.input.ref,
+        **snakemake.params
+    )
+
+    KAPy.write_variables(BA, dict(snakemake.output))
+
+
+# IMPORTANT: prevent execution during Dask worker re-import
+if "snakemake" in globals():
+    main()
