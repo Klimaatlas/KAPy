aa8e7d55b2faec1bcdae0dd6c4580599e9620551
Mark R. Payne <mapa@dmi.dk>
2026-05-26 16:38:32 +0200

    Remove support for modules in derived variables #186

---
+++ workflow/testing/temperature_conversions.py
@@ -0,0 +1,10 @@
+#Simple demonstration script showing how derived variables can be calculated
+
+import xclim
+
+def C2K(tas):
+    #Convert the units of tas to Kelvin, using xclim's unit handling function.
+    tasK=xclim.core.units.convert_units_to(tas ,"K")
+
+    return tasK
+
